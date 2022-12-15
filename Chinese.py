6cbc7c55428183fd67216f54d12abf16d1a8ad7a
Henry Meier <32826270+hcm444@users.noreply.github.com>
2022-12-15 01:17:57 -0800

    Delete Chinese.py

--- Chinese.py
+++
@@ -1,8 +0,0 @@
-# Prompt the user to enter a number
-number = int(input("Enter a number: "))
-
-# Check if the number is odd or even
-if number % 2 == 0:
-  print(number, "is an even number.")
-else:
-  print(number, "is an odd number.")
