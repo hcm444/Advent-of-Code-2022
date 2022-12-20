a109942d41f1e4e52b0e6c903f2949f67cd37274
Henry Meier <32826270+hcm444@users.noreply.github.com>
2022-12-20 12:02:42 -0800

    Create Day8.py

---
+++ Day8.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def show_heatmap(data):
+    # Normalize the data to range between 0 and 1
+    data = (data - np.min(data)) / (np.max(data) - np.min(data))
+
+    # Create a colormap
+    cmap = plt.get_cmap('jet')
+
+    # Display the heatmap
+    plt.imshow(data, cmap=cmap, interpolation='nearest')
+    plt.show()
+
+# Example data
+data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# Display the heatmap
+show_heatmap(data)
