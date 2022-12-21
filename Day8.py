4a4da36f2b87cdb63a47620748e10ed801a0f53d
bot <hcm1476@gmail.com>
2022-12-21 09:47:58 -0800

    Day 8 work

---
+++ Day8.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+file = open('Day8.txt', 'r').readlines()
+data = np.array([[int(x) for x in y.strip()] for y in file])
+plt.imshow(data)
+plt.xticks([])
+plt.yticks([])
+plt.show()
